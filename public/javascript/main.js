f04251105e2349b2137f20fdc37cc8e1662b5480
Andre Baptista <albs_br@yahoo.com.br>
2026-01-06 16:04:06 -0300

    v.0.11.0

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,5 @@
-console.log('Bulma Dashboard');
+console.log('Futstats');
+
 //Sidebar Collapse
 Array.from(document.getElementById('sidebar').getElementsByClassName('has-dropdown')).forEach(element => {
     element.addEventListener('click', function(e) {
@@ -9,3 +10,38 @@ Array.from(document.getElementById('sidebar').getElementsByClassName('has-dropdo
         event.preventDefault();
     }, element);
 })
+
+const API_URL = "https://71n5rm3blj.execute-api.sa-east-1.amazonaws.com/default/futstats-lambda";
+
+$(document).ready(function() {
+    
+    // TODO: put on menu click
+    $("#main").load("todos-os-jogos.html", loadTableTodosOsJogos);
+
+
+    // Function to load all items (Read operation)
+    function loadTableTodosOsJogos() {
+        $.ajax({
+            url: API_URL,
+            method: "GET",
+            dataType: "text",
+            success: function(items) {
+                $("#tableRows").empty();
+                console.info(items);
+                // $.each(items, function(index, item) {
+                //     $("#tableRows").append(`
+                //         <tr>
+                //             <th>${item.id}</th>
+                //             <td>${item.name}</td>
+                //             <td><button class="button editItem" data-id="${item.id}" data-name="${item.name}">Edit</button></td>
+                //             <td><button class="delete deleteItem" data-id="${item.id}"></button></td>
+                //         </tr>
+                //     `);
+                // });
+            },
+            error: function(xhr, status, error) {
+                console.error("Error loading:", error);
+            }
+        });
+    }
+});
